2278db53770de18becf24f2036798380a561cffe
code-vaibhav <vaibhavgoyal2506@gmail.com>
2021-10-01 18:16:52 +0530

    some fix

--- src/services/kratos.ts
+++ src/services/kratos.ts
@@ -1,11 +1,10 @@
 import { Configuration, V0alpha1Api } from "@ory/kratos-client";
 import instance from "./axios";
-import { kratosBase } from "../assets/settings";
 
 export const ory = new V0alpha1Api(
   new Configuration({
-    basePath: kratosBase,
+    basePath: process.env.REACT_APP_KRATOS_BASE,
   }),
-  kratosBase,
+  process.env.REACT_APP_KRATOS_BASE,
   instance
 );
